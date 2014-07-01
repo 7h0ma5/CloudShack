90f93ff3e70a051c5d1744423d3d4dedf8726ed6
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2014-07-02 01:13:54 +0200

    embed spinner template into directive

--- webapp/directives/spinner.js
+++ webapp/directives/spinner.js
@@ -1,6 +1,15 @@
 app.directive("spinner", function() {
+    var template = '\
+    <span class="spinner">\
+      <span class="rect1"></span>\
+      <span class="rect2"></span>\
+      <span class="rect3"></span>\
+      <span class="rect4"></span>\
+      <span class="rect5"></span>\
+    </span>';
+
     return {
          restrict: 'E',
-         templateUrl: "/partials/spinner.html"
+         template: template
     };
 });
