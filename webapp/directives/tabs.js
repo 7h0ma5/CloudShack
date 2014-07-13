bd835bda3ff4d8e074633786ebb17b65ee124a39
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2014-07-13 13:33:37 +0200

    implement tabs directive

---
+++ webapp/directives/tabs.js
@@ -0,0 +1,37 @@
+app.controller("TabsController", function($scope) {
+    $scope.current = 0;
+    $scope.show = function(index) {
+        $scope.current = index;
+    }
+});
+
+app.directive("tabs", function() {
+    function compile(element) {
+        element.addClass("tabs");
+        var tabs = angular.element("<ul>");
+        var index = 0;
+
+        angular.forEach(element.children(), function(child) {
+            var child = angular.element(child);
+            var link = angular.element("<a>");
+            var tab = angular.element("<li>").append(link);
+
+            tab.attr("ng-class", '{active: current == ' + index + '}');
+            link.attr("ng-click", "show(" + index + ")");
+            link.text(child.attr("title"));
+
+            child.attr("ng-show", "current == " + index);
+
+            tabs.append(tab);
+            index++;
+        });
+
+        element.prepend(tabs);
+    }
+
+    return {
+         restrict: 'E',
+         controller: 'TabsController',
+         compile: compile
+    };
+});
