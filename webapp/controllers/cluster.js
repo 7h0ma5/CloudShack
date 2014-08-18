7f115bdd562ad6c5dd58eff9a64f9f7fe21530f6
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2014-08-18 11:20:09 +0200

    implement frequency tuning from cluster

--- webapp/controllers/cluster.js
+++ webapp/controllers/cluster.js
@@ -1,3 +1,9 @@
-app.controller("ClusterCtrl", function($scope, $routeParams, Spots) {
+app.controller("ClusterCtrl", function($scope, $routeParams, Spots, Rig) {
     $scope.spots = Spots.get();
+
+    $scope.tune = function(idx) {
+        var spot = $scope.spots[idx];
+        var freq = parseInt(spot.freq);
+        Rig.setFrequency(Math.round(freq * 1000));
+    };
 });
