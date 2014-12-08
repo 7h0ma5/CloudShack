c5fd8d78fee2f1890a3cf5ad9dc665c03757ce2c
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2014-12-08 15:46:49 +0100

    Fixes statistics on the start page

--- webapp/controllers/home.js
+++ webapp/controllers/home.js
@@ -1,6 +1,6 @@
 app.controller("HomeCtrl", function($scope, Contact) {
     $scope.contactCount = 0;
     Contact.stats({group_level: 0}, function(stats) {
-        $scope.contactCount = stats.rows[0].doc;
+        $scope.contactCount = stats.rows[0].value;
     });
 });
