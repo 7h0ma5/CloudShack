55291212a18cf7314339867debc947ee0dac7e68
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2015-11-15 16:07:05 +0100

    Implement DXCC query on NewContactPage

--- webapp/app/pages/contact/new.ts
+++ webapp/app/pages/contact/new.ts
@@ -1,13 +1,14 @@
 import {Component, View, NgIf, NgFor, FORM_DIRECTIVES} from "angular2/angular2";
+import {Control} from "angular2/angular2";
+import {Http, Response} from "angular2/http";
 import {ContactService} from "../../services/contact";
-import {DxccService} from "../../services/dxcc";
 import {TAB_DIRECTIVES} from "../../components/tabs";
 import {Uppercase} from "../../components/uppercase";
 import {WorldMap} from "../../components/worldmap";
 import {MODES, CONTESTS} from "../../constants";
 
 @Component({
-    providers: [ContactService, DxccService]
+    providers: [ContactService]
 })
 @View({
     templateUrl: "/templates/contact/new.html",
@@ -17,17 +18,25 @@ export class NewContactPage {
     contact: Object = {};
     dxcc: Object = null;
     callbook: Object = null;
+    maptarget: [number, number] = null;
+    callsign: Control = new Control();
+
     startDate: Date = new Date();
     endDate: Date = new Date();
-    maptarget: [number, number] = null;
+
     modes = MODES;
     contests = CONTESTS;
 
     constructor(
         public api: ContactService,
-        public dxccService: DxccService
+        public http: Http
     ) {
-
+        this.callsign.valueChanges
+            .debounceTime(500)
+            .switchMap(val => this.http.get("/dxcc/" + val))
+            .filter((res: Response) => res.status == 200)
+            .map((res: Response) => res.json())
+            .subscribe((dxcc: any) => this.updateDxcc(dxcc));
     }
 
     qrz() {
@@ -40,6 +49,23 @@ export class NewContactPage {
 
     reset() {
 
+    }
+
+    updateDxcc(dxcc) {
+        this.dxcc = dxcc;
+
+        if (!dxcc) {
+            delete this.contact["dxcc"];
+            delete this.contact["cqz"];
+            delete this.contact["ituz"];
+            this.maptarget = null;
+            return;
+        }
+        console.log(dxcc);
+        this.contact["dxcc"] = dxcc["dxcc"];
+        this.contact["cqz"] = dxcc["cqz"];
+        this.contact["ituz"] = dxcc["ituz"];
+        this.maptarget = dxcc["latlon"];
     }
 
     sendCW(text) {
