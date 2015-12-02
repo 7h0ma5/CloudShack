c054e04016f43fb99d58fe5862a7dfdab9c840b5
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2015-12-02 14:44:57 +0100

    Initial dxcluster implementation

---
+++ lib/dxcluster/src/cluster.rs
@@ -0,0 +1,37 @@
+use std::net::TcpStream;
+use std::io::{BufReader, BufRead, Write};
+
+use Spot;
+
+pub struct Cluster {
+    addr: String,
+    user: Option<String>
+}
+
+impl Cluster {
+    pub fn new(addr: &str, user: Option<&str>) -> Cluster {
+        Cluster { addr: addr.to_owned(), user: user.map(|v| v.to_owned()) }
+    }
+
+    pub fn run(&self) {
+        let mut stream = TcpStream::connect(&*self.addr).unwrap();
+        let mut reader = BufReader::new(stream.try_clone().unwrap());
+
+        if let Some(ref user) = self.user {
+            stream.write_all(user.as_bytes()).unwrap();
+            stream.write_all(b"\n").unwrap();
+        }
+
+        loop {
+            let mut line = String::new();
+            let result = reader.read_line(&mut line);
+
+            if result.is_err() { break; }
+
+            if regex!(r"^DX").is_match(&*line) {
+                let spot = Spot::parse(&*line);
+                println!("new dx: {:?}", spot);
+            }
+        }
+    }
+}
--- lib/dxcluster/src/lib.rs
+++ lib/dxcluster/src/lib.rs
@@ -1,3 +1,15 @@
+#![feature(plugin)]
+#![plugin(regex_macros)]
+extern crate regex;
+
+mod cluster;
+mod spot;
+
+pub use cluster::Cluster;
+pub use spot::Spot;
+
 #[test]
 fn it_works() {
+    let cluster = Cluster::new("db0sue.de:8000", Some("DL2IC"));
+    cluster.run();
 }
