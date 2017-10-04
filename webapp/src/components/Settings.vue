c303fa3a63ba7fb2a532c863128b244ba13476bb
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2017-10-04 16:04:15 +0200

    Add Settings page

---
+++ webapp/src/components/Settings.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+    <v-tabs dark fixed centered class="elevation-1">
+      <v-tabs-bar>
+        <v-tabs-slider class="yellow"></v-tabs-slider>
+        <v-tabs-item href="#callbook">Callbook</v-tabs-item>
+        <v-tabs-item href="#cluster">DX Cluster</v-tabs-item>
+        <v-tabs-item href="#database">Database</v-tabs-item>
+        <v-tabs-item href="#sync">Sync</v-tabs-item>
+        <v-tabs-item href="#wsjt">WSJT</v-tabs-item>
+      </v-tabs-bar>
+      <v-tabs-items>
+        <v-tabs-content id="callbook">
+          <v-container grid-list-md>
+            <b>HamQTH</b>
+            <v-layout row>
+              <v-flex sm6>
+                <v-text-field
+                  label="Username"
+                  v-model="config.hamqth.user">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm6>
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  v-model="config.hamqth.user">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tabs-content>
+        <v-tabs-content id="cluster">
+          <v-container grid-list-md>
+            <v-layout row>
+               <v-flex sm4>
+                <v-text-field
+                  label="Host"
+                  v-model="config.cluster.host">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Port"
+                  type="number" step="1" min="1" max="65535"
+                  v-model="config.cluster.port">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="User"
+                  v-model="config.cluster.user">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tabs-content>
+        <v-tabs-content id="database">
+          <v-container grid-list-md>
+            <v-layout row>
+               <v-flex sm4>
+                <v-text-field
+                  label="Host"
+                  v-model="config.database.host">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Port"
+                  type="number" step="1" min="1" max="65535"
+                  v-model="config.database.port">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Protocol"
+                  v-model="config.database.protocol">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex sm4>
+                <v-text-field
+                  label="Name"
+                  v-model="config.database.name">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="User"
+                  v-model="config.database.user">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  v-model="config.database.password">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tabs-content>
+        <v-tabs-content id="sync">
+          <v-container grid-list-md>
+            <v-layout row>
+               <v-flex sm4>
+                <v-text-field
+                  label="User"
+                  v-model="config.sync.user">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Password"
+                  type="password"
+                  v-model="config.sync.password">
+                </v-text-field>
+              </v-flex>
+              <v-flex sm4>
+                <v-text-field
+                  label="Access Key"
+                  v-model="config.sync.key">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tabs-content>
+        <v-tabs-content id="wsjt">
+          <v-container grid-list-md>
+            <v-layout row>
+               <v-flex sm1>
+                <v-text-field
+                  label="Port"
+                  type="number" step="1" min="1" max="65535"
+                  v-model="config.wsjt.port">
+                </v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-tabs-content>
+      </v-tabs-items>
+    </v-tabs>
+
+    <p>
+      <v-btn primary
+             :loading="saving"
+             :disabled="saving"
+             @click="save()">
+        {{$t("save")}}
+      </v-btn>
+
+      <v-btn error @click="reset()">
+        {{$t("reset")}}
+      </v-btn>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      config: {
+        hamqth: {},
+        cluster: {},
+        database: {},
+        sync: {},
+        wsjt: {}
+      },
+      saving: false
+    }
+  },
+  created: function() {
+    this.$http.get("config").then((response) => {
+      this.config = response.data
+    })
+  },
+  methods: {
+    save: function() {
+      this.saving = true
+      this.$http.post("config", JSON.stringify(this.config)).then((response) => {
+        this.saving = false
+      })
+    }
+  }
+}
+</script>
--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -3,6 +3,7 @@ import Router from "vue-router"
 import Home from "@/components/Home"
 import Logbook from "@/components/Logbook"
 import NewContact from "@/components/NewContact"
+import Settings from "@/components/Settings"
 import Wsjt from "@/components/Wsjt"
 
 Vue.use(Router)
@@ -23,6 +24,11 @@ export default new Router({
       path: "/contacts/new",
       name: "NewContact",
       component: NewContact
+    },
+    {
+      path: "/settings",
+      name: "Settings",
+      component: Settings
     },
     {
       path: "/wsjt",
