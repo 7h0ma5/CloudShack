96c6a5e2b3b927786db54b685927629d7d21dbc2
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2018-03-31 21:45:44 +0200

    Update versions

---
+++ webapp/src/components/Export.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <div>
+      <div>
+        <v-btn color="primary" :href="link">Export</v-btn>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import _ from "lodash"
+
+export default {
+  name: "export",
+  data: function() {
+    return {
+    }
+  },
+  computed: {
+    link: function() {
+      return "/contacts/_adi"
+    }
+  }
+}
+</script>
